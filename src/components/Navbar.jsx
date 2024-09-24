73958f9053666dd705b52e007649fe6968879df4
ZenChris <121231422+ZenChris18@users.noreply.github.com>
2024-09-24 14:10:29 +0800

    removed twitter and instagram icons, made github/linkedin/email clickable

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,16 +8,33 @@ const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
-        <img className="mx-2 w-14"src={logo} alt="logo" />
+        <img className="mx-2 w-14" src={logo} alt="logo" />
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <FaLinkedin />
-        <FaGithub />
-        <FaInstagram />
-        <FaSquareXTwitter />
+        {/* LinkedIn Icon */}
+        <a
+          href="https://www.linkedin.com/in/yujison-chriszen-lagamayo/" 
+          target="_blank"
+          rel="noopener noreferrer" 
+          aria-label="LinkedIn"
+        >
+          <FaLinkedin />
+        </a>
+
+        {/* GitHub Icon */}
+        <a
+          href="https://github.com/zenchris18" 
+          target="_blank"
+          rel="noopener noreferrer" //security reason
+          aria-label="GitHub"
+        >
+          <FaGithub />
+        </a>
+
+
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
