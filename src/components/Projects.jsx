5efee8c102b64bc49d9df555dea611b42a13c699
ZenChris <121231422+ZenChris18@users.noreply.github.com>
2024-09-24 14:22:40 +0800

    changed project image size

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,8 +11,8 @@ const Projects = () => {
                     <motion.div whileInView={{opacity:1, x:0}} initial={{opacity:0, x:-100}} transition={{duration:1}} className="w-full lg:w-1/4">
                         <img 
                             src={project.image} 
-                            width={150} 
-                            height={150}
+                            width={300} 
+                            height={300}
                             alt={project.title} 
                             className="mb-6 rounded"
                         />
